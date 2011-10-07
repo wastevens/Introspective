d6a61bc9d94218d3725665a69797f4c00f4f2c6f
Walter Stevens <wastevens@gmail.com>
2011-10-07 13:56:52 -0400

    Housekeeping

--- src/test/java/testsupport/pojo/A.java
+++ src/test/java/testsupport/pojo/A.java
@@ -7,18 +7,7 @@ public class A extends Letter {
     @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"})
     private final String label;
 
-    @Foo(value = "Field Annotation")
-    @Bar(value = "Another Field Annotation")
-    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"})
-    private final Integer annotatedField;
-
     public A(String label) {
         this.label = label;
-        annotatedField = 0;
-    }
-
-    @Foo(value = "Method Annotation")
-    @Bar(value = "Another Method Annotation")
-    public void someMethod(@Foo("Arguement Annotation") @Bar("Another Arguement Annotation") String anArguement) {
     }
 }
